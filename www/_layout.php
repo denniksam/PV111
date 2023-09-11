f8a2df43667dd26fdea4aa321c96057d74350023
Denys Samoilenko <denniksam@gmail.com>
2023-09-11 19:35:03 +0300

    lesson 1
    lesson 1

---
+++ www/_layout.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>PV-111</title>
+	<!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<!--Import Google Icon Font-->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+	<nav>
+		<div class="nav-wrapper orange">
+		  <a href="/" class="brand-logo">PV-111</a>
+		  <ul id="nav-mobile" class="right hide-on-med-and-down">
+			<li><a href="sass.html">Sass</a></li>
+			<li><a href="badges.html">Components</a></li>
+			<li><a href="collapsible.html">JavaScript</a></li>
+		  </ul>
+		</div>
+	</nav>
+	<div class="container">
+		<?php include $page ; ?>
+	</div>
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
--- www/access_manager.php
+++ www/access_manager.php
@@ -1,10 +1,12 @@
 <?php
 
 $uri = $_SERVER[ 'REQUEST_URI' ] ;  // адреса запиту 
-if( $uri == 'index' ) {
-	include 'index.php' ;  # 
+if( $uri == '/index' || $uri === '/about' ) {
+	$page =  // змінні локалізуються тільки у функціях, оголошена поза функцією змінна доступна скрізь, у т.ч. в іншому файлі
+		substr( $uri, 1 ) . '.php' ;  // у РНР оператор "+" діє тільки на числа, для рядків - оператор "."
+	include '_layout.php' ;  // перехід до інструкцій в іншому файлі
 }
 else {
-	echo 'acces manager - 404' ;
+	echo 'access manager - 404' ;
 }
 
